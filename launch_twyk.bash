8f762d296aab1abe6ae32c79a5c4b34d9287c20f
Jack Kolb <jack@jackkolb.com>
2021-12-01 19:59:04 -0500

    Shell script

--- launch_twyk.bash
+++ launch_twyk.bash
@@ -1,16 +1,16 @@
 SCRIPT_SERVER="
               cd ~/Documents/TWYK;
-              python server/server.py
+              python server/server.py;
               "
 
 SCRIPT_MD="
               cd ~/Documents/TWYK;
-              python marker_detection.py
+              python marker_detection.py;
               "
 
 SCRIPT_STUDY="
               cd ~/Documents/TWYK;
-              python study_controller.py
+              python study_controller.py;
               "
 
 # Terminal 1
@@ -26,3 +26,9 @@ sleep 1
 # Terminal 3
 gnome-terminal --tab -- bash -c "${SCRIPT_STUDY}"
 
+# Firefox admin
+firefox --new-window https://docs.google.com/forms/d/e/1FAIpQLSenPlvRary8AOlOjV1utXl7GDbFfCjC159l6srm9wSaihKAUg/viewform &
+firefox --new-window 127.0.0.1:5000/admin &
+
+# Firefox display
+firefox --new-window 127.0.0.1:5000/ &
